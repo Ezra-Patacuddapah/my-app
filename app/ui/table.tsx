50d5cb831594599b9d95280a5632d8baf758a483
Ezra Patacuddapah <ezrapatacuddapah@gmail.com>
2025-11-13 15:42:50 +0300

    adjusted the layout of main view

--- app/ui/table.tsx
+++ app/ui/table.tsx
@@ -13,7 +13,7 @@ export default async function Table({
     return (
         <div>
             {texts?.map((text:Text) => (
-                <div key={text.id} className="flex justify-center items-center border border-gray-300 rounded-md mx-1 my-2">
+                <div key={text.id} className="flex justify-center items-center border border-gray-600 rounded-md mx-1 my-2 py-8">
                     <p className="text-2xl">{text.text}</p>
                 </div>
             ))}
