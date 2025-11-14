2e6a46019e08f8f92d9aca0e65c161fec21f88ab
Ezra Patacuddapah <ezrapatacuddapah@gmail.com>
2025-11-14 12:09:29 +0300

    added responsiveness of search and text cards

--- app/ui/admin/table.tsx
+++ app/ui/admin/table.tsx
@@ -16,7 +16,7 @@ export default async function Table({
         <div className='grid grid-cols-1 md:grid-cols-3 md:px-30 gap-3 mt-3'>
             {texts?.map((text:Text) => (
                 <div key={text.id} className='flex flex-col justify-between items-center border border-gray-600 rounded-md p-2'>
-                    <p className="text-2xl">{text.text}</p>
+                    <p className="md:text-2xl">{text.text}</p>
                     <div className='flex justify-center items-center'>
                         <Update id={text.id} />
                         <Delete id={text.id} />
--- app/ui/table.tsx
+++ app/ui/table.tsx
@@ -14,7 +14,7 @@ export default async function Table({
         <div className='grid grid-cols-1 md:grid-cols-3 md:px-30 gap-3 mt-3'>
             {texts?.map((text:Text) => (
                 <div key={text.id} className="flex justify-center items-center border border-gray-600 rounded-md p-6">
-                    <p className="text-2xl">{text.text}</p>
+                    <p className="md:text-2xl">{text.text}</p>
                 </div>
             ))}
         </div>
